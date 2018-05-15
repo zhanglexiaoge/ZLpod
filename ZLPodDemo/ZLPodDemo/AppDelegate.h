6d43e499dbacf57778f619f7c11c8d5ab2b10a3b
zl <miugodigital@MiugodigitaldeMacBook-Air.local>
2018-05-15 15:02:06 +0800

    1

---
+++ ZLPodDemo/ZLPodDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZLPodDemo
+//
+//  Created by Miugodigital on 2018/5/15.
+//  Copyright © 2018年 zhangle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZLPodDemo/ZLPodDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZLPodDemo
+//
+//  Created by Miugodigital on 2018/5/15.
+//  Copyright © 2018年 zhangle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
